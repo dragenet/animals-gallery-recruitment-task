1645bb55be01c0ab23840f3a6482c9bfd5416c77
Dragenet <dragenetdev@gmail.com>
2023-03-22 18:17:28 +0100

    Revert "Setup project"

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.ts',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      provider: 'c8',
-    },
-  },
-});
+})
