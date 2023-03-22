cfab2324140e4e5e0e61f74d39bc9f352831677e
Dominik Domagalski <dragenetdev@gmail.com>
2023-03-22 17:22:43 +0100

    Add vitest with react-testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'c8',
+    },
+  },
 });
