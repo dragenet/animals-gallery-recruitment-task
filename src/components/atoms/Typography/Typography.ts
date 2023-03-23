b702ac1f94b687de34dc022109054d351fcd963b
Dominik Domagalski <dragenetdev@gmail.com>
2023-03-23 16:01:00 +0100

    Add Typography components

---
+++ src/components/atoms/Typography/Typography.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { TypographyProps } from './types';
+import { typographyMixin } from './utils';
+
+export const H1 = styled.h1<TypographyProps>`
+  ${typographyMixin({ fontSize: 'large' })}
+`;
+
+export const Subtitle = styled.p<TypographyProps>`
+  ${typographyMixin({ fontSize: 'small', bold: true })}
+`;
