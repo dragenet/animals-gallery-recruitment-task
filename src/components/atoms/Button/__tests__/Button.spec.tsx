fb0118a35903ac63cfad1924ffeb183fd7ccf125
Dominik Domagalski <dragenetdev@gmail.com>
2023-03-24 15:32:26 +0100

    Add @testing-library/user-event Add more tests to button including clicks Add package.json script to update snapshots

--- src/components/atoms/Button/__tests__/Button.spec.tsx
+++ src/components/atoms/Button/__tests__/Button.spec.tsx
@@ -1,4 +1,5 @@
-import { describe, expect, it } from 'vitest';
+import { screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 
 import { Button } from '~/components/atoms/Button';
 import { renderTest } from '~/utils/renderTest';
@@ -8,5 +9,24 @@ describe('Button', () => {
     const { container } = renderTest(<Button>Hello there!</Button>);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it('Secondary renders correctly', () => {
+    const { container } = renderTest(<Button variant="secondary">Hello there secondary!</Button>);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('is clickable', async () => {
+    const handleClickSpy = vi.fn();
+    const { user } = renderTest(
+      <Button variant="secondary" onClick={handleClickSpy}>
+        Hello there secondary!
+      </Button>,
+    );
+
+    await user.click(screen.getByRole('button'));
+
+    expect(handleClickSpy).toHaveBeenCalledOnce();
   });
 });
